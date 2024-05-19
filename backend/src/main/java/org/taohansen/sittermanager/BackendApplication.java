32d8f11dffec523b62185365484bf8c6a20347e9
Tao Hansen <taohansens@gmail.com>
2024-05-19 17:00:06 -0300

    add swagger and h2-database;

--- backend/src/main/java/org/taohansen/sittermanager/BackendApplication.java
+++ backend/src/main/java/org/taohansen/sittermanager/BackendApplication.java
@@ -1,13 +1,15 @@
 package org.taohansen.sittermanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Sitter Manager", version = "1.0", description = "Sitter Manager API"))
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
