8c5303c6c9a32bf02cb667cd53bc956c1de3d14e
Tao Hansen <taohansens@gmail.com>
2024-05-20 23:29:41 -0300

    include links homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Babysitter Manager</h1>
+    <div className={"p-10"}>
+      <h1 className={"font-bold"}>Babysitter Manager</h1>
+      <p><Link href="/babysitters"> Babysitter </Link></p>
+      <p><Link href="/roles"> Roles </Link></p>
+          <p><Link href="/users"> Users </Link></p>
     </div>
   );
 }
